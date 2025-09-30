0681993f809030eac2367de1a901228110f0c779
akbarlucu <rachmadrizqiakbarsyah@gmail.com>
2025-10-01 01:15:55 +0700

    add defer

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -2,31 +2,60 @@ package productcontroller
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/akbarlucu/go-rest-api/helper"
 	"github.com/akbarlucu/go-rest-api/models"
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 )
+
+var ResponseJson = helper.ResponseJson
+var ResponseError = helper.ResponseError
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	var products []models.Product
 
 	if err := models.DB.Find(&products).Error; err != nil {
-		fmt.Println(err)
+		ResponseError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-
-	response, _ := json.Marshal(products)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(response)
+	ResponseJson(w, http.StatusOK, products)
 }
 
 func Show(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		ResponseError(w, http.StatusBadRequest, err.Error())
+		return
+	}
 
+	var product models.Product
+	if err := models.DB.First(&product, id).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			ResponseError(w, http.StatusNotFound, "Product Tidak Ditemukan")
+			return
+		default:
+			ResponseError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+	}
+
+	ResponseJson(w, http.StatusOK, product)
 }
 
 func Create(w http.ResponseWriter, r *http.Request) {
+
+	var product models.Product
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&product); err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
 
 }
 
