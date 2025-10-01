c9f935e2a6b261039d911710ee1d32d72d7d033f
akbarlucu <rachmadrizqiakbarsyah@gmail.com>
2025-10-01 22:10:22 +0700

    update

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -57,10 +57,41 @@ func Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	defer r.Body.Close()
+	if err := models.DB.Create(&product).Error; err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ResponseJson(w, http.StatusCreated, product)
 }
 
 func Update(w http.ResponseWriter, r *http.Request) {
 
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		ResponseError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	var product models.Product
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&product); err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := models.DB.Where("id= ?", id).Updates(&product).Error; err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	product.Id = id
+
+	ResponseJson(w, http.StatusOK, product)
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
